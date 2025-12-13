fd46620e2265ccf777543578baa4a5975dd32798
yunchan <yunchan771@gmail.com>
2025-12-13 13:48:00 +0000

    study day1

--- Udemy/Day 1/band_name.py
+++ Udemy/Day 1/band_name.py
@@ -1,1 +1,8 @@
-print('Hello World')
+#Band name Generator
+
+print("Welcome to the Band Name Generator.\n")
+print("What's the name of the city you grew up in?")
+city=input()
+print("What's your pet's name?")
+pet_name=input()
+print("Your band name could be " + city + " " + pet_name)

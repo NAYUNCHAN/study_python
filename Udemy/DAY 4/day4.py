64f0451812b6204918c0796a0216fb7c151bb470
yunchan <yunchan771@gmail.com>
2025-12-27 06:04:18 +0000

    learn

---
+++ Udemy/DAY 4/day4.py
@@ -0,0 +1,18 @@
+import random 
+
+# random_int = random.randint(1,10)
+# print(random_int)
+
+# random_num = random.random()
+# print(random_num)
+
+# random_float = random.uniform(1,10)
+# print(random_float)
+
+# coin_random = random.randint(0,1)
+# if coin_random == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+
